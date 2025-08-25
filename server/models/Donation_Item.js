61c7aac63a60e82dae44a3408ed5bdb7e3ce5c49
Amishi Gupta <amishigupta@Amishis-MacBook-Air.local>
2025-08-25 11:40:51 +0530

    Models done

---
+++ server/models/Donation_Item.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+
+const donar_itemSchema = new mongoose.Schema({
+    itemName: { type: String, required: true },
+    quantity: { type: String, required: true },
+    imageFileId: { type: mongoose.Schema.Types.ObjectId },
+    createdAt: { type: Date, default: Date.now },
+    description: {type: String},
+    status: {
+        type: String,
+        enum: ['pending', 'approved', 'rejected', 'completed'],
+                default: 'pending',
+              },
+    category:{
+                type: String,
+                enum: ['Clothes', 'Bags', 'Stationary', 'Toys','Furniture','Books']
+              },
+    donorID:{
+        type:mongoose.Schema.Types.ObjectId, 
+        ref:'Donor'
+    }
+})
+
+const DonorItemModel =  mongoose.model('Donor Item', donor_itemSchema);
+
+export default DonorItemModel;
